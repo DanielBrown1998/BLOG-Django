694859933613922cabab6aed10c7ecc15f9611dd
Daniel <daniel_academico1998@hotmail.com>
2025-01-22 12:47:25 -0300

    Implement pagination and enhance blog layout; add new post and page templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,33 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
+
+posts = list(range(1000))
 # Create your views here.
 
+def post(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        #'page_obj': page_obj
+    }
+    return render(request, 'blog/pages/post.html', context)
+
+def page(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        #'page_obj': page_obj
+    }
+    return render(request, 'blog/pages/page.html', context)
+
 def index(request):
-    return render(request, 'blog/pages/index.html')
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'blog/pages/index.html', context)
